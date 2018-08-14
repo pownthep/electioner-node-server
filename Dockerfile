d15da6f0fd30f327ed13d45f0a5910e55ef5b7b9
Pownthep Laokhunthot <pownthep@Powntheps-MacBook-Pro.local>
2018-08-14 14:49:55 +0700

    Added Node js and deleted useless files

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,16 @@
-FROM php:7.0-apache
-EXPOSE 80
+FROM node:8
+
+# Create app directory
+WORKDIR /usr/src/app
+
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+# where available (npm@5+)
+COPY . .
+
+RUN npm install
+# If you are building your code for production
+# RUN npm install --only=production
+
+EXPOSE 8080
+CMD [ "npm", "start" ]
---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'html')))
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + 'html/index.html'));
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
