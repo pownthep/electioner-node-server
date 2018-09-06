861a6313a581362bb11fc0b9ac3d9738347c5154
Pownthep Laokhunthot <pownthepofficial@gmail.com>
2018-09-06 13:01:03 +0700

    Customize background and login form

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
     var particles = Particles.init({
+        maxParticles: 20,
         selector: '.background',
-        color: '#DA0463'
+        color: ['#005792', '#c5e5e3'],
+        connectParticles: true,
+        responsive: [
+            {
+              breakpoint: 768,
+              options: {
+                maxParticles: 0
+                }
+            }
+        ]
     });
+
 });
